bcaa98c86a753c2f14e654560fbc6052d6398ed4
jorge <jorge@futr.ai>
2020-05-09 19:13:16 +0100

    fixed chrome ext load

--- chrome-ext/src/js/background.js
+++ chrome-ext/src/js/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'bbc.co.uk'},
+          pageUrl: {hostEquals: 'developer.chrome.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
