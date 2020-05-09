1effeb1ef2e0ae3e76b40ce44fa974955813eb3c
jorge <jorge@futr.ai>
2020-05-09 18:58:02 +0100

    new logos

--- chrome-ext/src/js/background.js
+++ chrome-ext/src/js/background.js
@@ -1,13 +1,13 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
+  chrome.storage.sync.set({ color: '#3aa757' }, function() {
     console.log("The color is green.");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {hostEquals: 'bbc.co.uk'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
