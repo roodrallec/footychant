09776d61dbe101a929bc29d64c4371f1dea6e2c7
jorge <jorge@futr.ai>
2020-05-10 19:10:54 +0100

    scraped all urls for top 5 leagues in the world

--- scraper/src/main.js
+++ scraper/src/main.js
@@ -1,7 +1,13 @@
-const axios = require("axios")
+const axios = require('axios');
 const $ = require('cheerio');
-
 const baseUrl = "https://www.fanchants.com";
+const leagues = [
+    "/football-league/german-bundesliga-1/",
+    "/football-league/premiership/",
+    "/football-league/spanish-liga-1/",
+    "/football-league/italy-serie/",
+    "/football-league/french-ligue-1/"
+];
 
 async function scrapeChantsFromPage(url) {
     const html = await axios.get(url);
@@ -11,6 +17,26 @@ async function scrapeChantsFromPage(url) {
     return relativeUrls.map(el => baseUrl + el);
 }
 
-scrapeChantsFromPage("https://www.fanchants.com/football-team/borussia-dortmund/").then(
-    chants=> console.log(chants)
-);
+async function scrapeChantsFromLeague(leagueUrl) {
+    const html = await axios.get(leagueUrl);
+    const page = $.load(html.data);
+    const chantContainers = page('a');
+    return chantContainers.map(function(el,i) {
+        return $(this).attr("href")
+    }).get().filter(url => url.includes("/football-team/"));
+}
+
+async function scrapeAllLeagues() {
+    const data = {};
+    await Promise.all(leagues.map(async league => {
+        data[league] = {};
+        const teamUrls = await scrapeChantsFromLeague(baseUrl + league);
+        await Promise.all(teamUrls.map(async teamUrl => {
+            let chants = await scrapeChantsFromPage(baseUrl + teamUrl);
+            data[league][teamUrl] = chants;
+        }));
+    }));
+    console.log(JSON.stringify(data));
+}
+
+scrapeAllLeagues();
