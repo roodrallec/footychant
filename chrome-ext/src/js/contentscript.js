5fa8ea6925f398bee3bda34c10d213d87b3f8a95
jorge <jorge@futr.ai>
2020-05-10 11:24:02 +0100

    msg passing

--- chrome-ext/src/js/contentscript.js
+++ chrome-ext/src/js/contentscript.js
@@ -3,13 +3,24 @@
 */
 var docEl = (document.head||document.documentElement);
 var getUserMediaOverload = document.createElement('script');
-var audio = chrome.runtime.getURL("src/assets/chant.mp3");
+// var audio = chrome.runtime.getURL("src/assets/chant.mp3");
 
-getUserMediaOverload.textContent = "\
-console.log('playing sounds');\
-var a = new Audio(\
-    '" + audio + "'\
-);\
-a.play();\
-";
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener((msg) => {
+    console.log('msg received', msg);
+    // if (msg.function == 'html') {
+    //   port.postMessage({
+    //     html: document.documentElement.outerHTML,
+    //     description: document.querySelector("meta[name=\'description\']").getAttribute('content'),
+    //     title: document.title });
+    // }
+  });
+});
+// getUserMediaOverload.textContent = "\
+// console.log('playing sounds');\
+// var a = new Audio(\
+//     '" + audio + "'\
+// );\
+// a.play();\
+// ";
 docEl.appendChild(getUserMediaOverload);
--- chrome-ext/src/js/popup.js
+++ chrome-ext/src/js/popup.js
@@ -1,8 +1,19 @@
 let changeColor = document.getElementById("changeColor");
-changeColor.onclick = function (element) {
-    console.log("clicked button");
-};
-chrome.storage.sync.get("team", function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute("value", "BD");
-});
+function connect() {
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		const port = chrome.tabs.connect(tabs[0].id);
+		port.postMessage({ function: 'html' });
+		port.onMessage.addListener((response) => {
+			html = response.html;
+			title = response.title;
+			description = response.description;
+		});
+	});
+}
+window.addEventListener('load', (event) => {
+	chrome.tabs.executeScript(null, {
+	  file: 'src/js/contentscript.js',
+	}, () => {
+		connect() //this is where I call my function to establish a connection     });
+	});
+  });
